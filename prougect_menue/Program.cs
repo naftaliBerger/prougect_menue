ea1b0aa0fa06a109853112078bab63718602eb6a
Naftali Berger <bn26665@gmail.com>
2025-05-12 15:58:07 +0300

    start

---
+++ prougect_menue/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prougect_menue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
